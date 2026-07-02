88f33bad4c5429c5e0fc6afc9b5bd957936ccc5f
lakshmancharubsc-blip <lakshmancharubsc@gmail.com>
2026-07-02 16:52:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,37 @@
-<div className="background-image">
-  <div className="landing-page">
-    <h1>Paradise Nursery</h1>
-    <button className="get-started-btn">Get Started</button>
-  </div>
-</div>
+import React, { useState } from "react";
+import "./App.css";
+import ProductList from "./components/ProductList";
+
+function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
+  return (
+    <div>
+      {!showProductList ? (
+        <div className="background-image">
+          <div className="landing-page">
+            <div>
+              <h1>Paradise Nursery</h1>
+
+              <p>
+                Welcome to Paradise Nursery, your destination for beautiful
+                indoor plants and gardening essentials.
+              </p>
+
+              <button
+                className="get-started-btn"
+                onClick={() => setShowProductList(true)}
+              >
+                Get Started
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <ProductList />
+      )}
+    </div>
+  );
+}
+
+export default App;
